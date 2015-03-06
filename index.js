d5cb5ab0f09141352fd65258d7a53354f74164c4
Vinson Chuong <vchuong@pivotal.io>
2015-03-05 18:00:55 -0800

    refactor index.js to reuse server and stream code, make port configurable

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ var through = require('through2');
 var express = require('express');
 var SpecRunner = require('./lib/spec_runner');
 
+var DEFAULT_JASMINE_PORT = 8888;
+
+function createServer(options) {
+  var port = options && options.port || DEFAULT_JASMINE_PORT;
+
+  var files = {};
+  var stream = through.obj(function(file, encoding, callback) {
+    files[file.path] = file.contents;
+    callback();
+  });
+  stream.pause();
+
+  var app = express();
+  var server = app.listen(port, function() {
+    stream.resume();
+  });
+  app.get('/', function(req, res) {
+    res.redirect('/specRunner.html')
+  });
+  app.get('*', function(req, res) {
+    res.send(files[req.path].toString());
+  });
+  return {stream: stream, server: server};
+}
+
 exports.specRunner = function(options) {
   var specRunner = new SpecRunner(options);
   return through.obj(function(file, encoding, callback) {
@@ -14,53 +39,24 @@ exports.specRunner = function(options) {
   });
 };
 
-exports.phantomjs = function() {
-  var app = express();
-  var stream = through.obj(function(file, encoding, callback) {
-    app.get(file.path, function(req, res) {
-      res.send(file.contents.toString());
-    });
-    callback();
-  }, function(callback) {
-    var phantomProcess = childProcess.spawn('phantomjs', ['phantom_runner.js'], {
+exports.phantomjs = function(options) {
+  var port = options && options.port || DEFAULT_JASMINE_PORT;
+
+  var container = createServer(options);
+  container.stream.on('end', function() {
+    var phantomProcess = childProcess.spawn('phantomjs', ['phantom_runner.js', port], {
       cwd: path.resolve(__dirname, 'lib'),
       stdio: 'pipe'
     });
     phantomProcess.on('close', function() {
-      callback();
-      server.close();
+      container.server.close();
     });
     phantomProcess.stdout.pipe(process.stdout);
     phantomProcess.stderr.pipe(process.stderr);
   });
-  stream.pause();
-
-  var server = app.listen(8888, function() {
-    stream.resume();
-  });
-  app.get('/', function(req, res) {
-    res.redirect('/specRunner.html')
-  });
-  return stream;
+  return container.stream;
 };
 
-exports.server = function() {
-  var files = {};
-  var stream = through.obj(function(file, encoding, callback) {
-    files[file.path] = file.contents;
-    callback();
-  });
-  stream.pause();
-
-  var app = express();
-  app.listen(8888, function() {
-    stream.resume();
-  });
-  app.get('/', function(req, res) {
-    res.redirect('/specRunner.html')
-  });
-  app.get('*', function(req, res) {
-    res.send(files[req.path].toString());
-  });
-  return stream;
+exports.server = function(options) {
+  return createServer(options).stream;
 };
