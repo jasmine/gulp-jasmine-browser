224774ad984da1d8ea1340c020a6851455e9a7d8
Ryan Dy <rdy@pivotal.io>
2015-05-08 12:03:37 -0700

    Convert SpecRunner to a Class
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- src/lib/spec_runner.js
+++ src/lib/spec_runner.js
@@ -9,53 +9,56 @@ function resolveJasmineFiles(directoryProp, fileNamesProp) {
   return fileNames.map(fileName => path.resolve(directory, fileName));
 }
 
-function SpecRunner(options = {}) {
-  File.call(this, {path: '/specRunner.html', base: '/'});
+const inlineTagExtensions = {'.css': 'style', '.js': 'script'};
 
-  this.contents = new Buffer('<!DOCTYPE html>');
-  this.files = {};
+var privates = new WeakMap();
+class SpecRunner extends File {
+  constructor(options = {}) {
+    super({path: '/specRunner.html', base: '/'});
 
-  var files = [].concat(
-    resolveJasmineFiles('path', 'cssFiles'),
-    resolveJasmineFiles('path', 'jsFiles'),
-    options.console ? ['console.js', 'console_boot.js'] : resolveJasmineFiles('bootDir', 'bootFiles')
-  );
-  files.forEach(fileName => this.inlineFile(fileName));
-}
+    this.contents = new Buffer('<!DOCTYPE html>');
+    privates.set(this, {files: new Set()});
 
-SpecRunner.prototype = Object.create(File.prototype);
-
-var inlineTagExtensions = {'.css': 'style', '.js': 'script'};
-SpecRunner.prototype.inlineFile = function(filePath) {
-  var fileContents = fs.readFileSync(path.resolve(__dirname, filePath), {encoding: 'utf8'});
-  var fileExtension = inlineTagExtensions[path.extname(filePath)];
-
-  this.contents = Buffer.concat([
-    this.contents,
-    new Buffer(`<${fileExtension}>${fileContents}</${fileExtension}>`)
-  ]);
-  return this;
-};
-
-SpecRunner.prototype.addFile = function(filePath) {
-  if (this.files[filePath]) return this;
-  this.files[filePath] = true;
-
-  var fileExtension = path.extname(filePath);
-
-  var html = '';
-  if (fileExtension === '.js') {
-    html = `<script src="${filePath}"></script>`;
-  } else if (fileExtension === '.css') {
-    html = `<link href="${filePath}" rel="stylesheet" type="text/css"></link>`;
+    var files = [].concat(
+      resolveJasmineFiles('path', 'cssFiles'),
+      resolveJasmineFiles('path', 'jsFiles'),
+      options.console ? ['console.js', 'console_boot.js'] : resolveJasmineFiles('bootDir', 'bootFiles')
+    );
+    files.forEach(fileName => this.inlineFile(fileName));
   }
 
-  this.contents = Buffer.concat([
-    this.contents,
-    new Buffer(html)
-  ]);
+  inlineFile(filePath) {
+    var fileContents = fs.readFileSync(path.resolve(__dirname, filePath), {encoding: 'utf8'});
+    var fileExtension = inlineTagExtensions[path.extname(filePath)];
 
-  return this;
-};
+    this.contents = Buffer.concat([
+      this.contents,
+      new Buffer(`<${fileExtension}>${fileContents}</${fileExtension}>`)
+    ]);
+    return this;
+  }
+
+  addFile(filePath) {
+    var {files} = privates.get(this);
+    if (files.has(filePath)) return this;
+    files.add(filePath);
+
+    var fileExtension = path.extname(filePath);
+
+    var html = '';
+    if (fileExtension === '.js') {
+      html = `<script src="${filePath}"></script>`;
+    } else if (fileExtension === '.css') {
+      html = `<link href="${filePath}" rel="stylesheet" type="text/css"></link>`;
+    }
+
+    this.contents = Buffer.concat([
+      this.contents,
+      new Buffer(html)
+    ]);
+
+    return this;
+  }
+}
 
 module.exports = SpecRunner;
