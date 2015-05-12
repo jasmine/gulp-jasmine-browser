63881c724ff690034cb79f110806c1a7b3854629
Ryan Dy <rdy@pivotal.io>
2015-05-12 14:58:29 -0700

    try more process signal messages for travis-ci
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -23,7 +23,20 @@ describe('gulp-jasmine-browser', function() {
     var process = childProcess.exec([gulpPath, '--gulpfile', gulpFile, task].join(' '),
       {timeout}, (error, stdout, stderr) => resolveCompleted({error, stdout, stderr}));
 
-    var closed = new Promise(resolve => process.on('close', resolve));
+    var closed = new Promise(function(resolve) {
+      process.on('close', function() {
+        console.log('close');
+        resolve();
+      });
+      process.on('SIGTERM', function() {
+        console.log('SIGTERM');
+        resolve();
+      });
+      process.on('uncaughtException', function() {
+        console.log('uncaughtException');
+        resolve();
+      });
+    });
 
     processes.push({process, closed});
 
