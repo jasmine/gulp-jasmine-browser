918f767a4dfdedc970a968d5f9fb1f15edd398b6
Ryan Dy <rdy@pivotal.io>
2016-03-27 20:27:28 -0700

    make sure the lint task has base directory

--- tasks/lint.js
+++ tasks/lint.js
@@ -3,7 +3,7 @@ const {eslint, 'if': gulpIf} = require('gulp-load-plugins')();
 
 gulp.task('lint', () => {
   const {FIX: fix = true} = process.env;
-  return gulp.src(['gulpfile.js', 'tasks/**/*.js', 'src/**/*.js', 'spec/**/*.js', '!src/lib/console.js'])
+  return gulp.src(['gulpfile.js', 'tasks/**/*.js', 'src/**/*.js', 'spec/**/*.js', '!src/lib/console.js'], {base: '.'})
     .pipe(eslint({fix}))
     .pipe(eslint.format('stylish'))
     .pipe(gulpIf(file => file.eslint && typeof file.eslint.output === 'string', gulp.dest('.')))
