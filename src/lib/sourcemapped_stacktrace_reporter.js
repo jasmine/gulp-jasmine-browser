3e5fb12c09464b746db131c355625e15394809fc
Ryan Dy <rdy@pivotal.io>
2016-04-06 00:15:20 -0700

    filter out lines that do not have js files

--- src/lib/sourcemapped_stacktrace_reporter.js
+++ src/lib/sourcemapped_stacktrace_reporter.js
@@ -5,6 +5,11 @@
       for(var i = 0; i < traces.length; i++) {
         (function(node){
           sourceMappedStackTrace.mapStackTrace(node.textContent, function(stack) {
+            if (stack.filter) {
+              stack = stack.filter(function(line) {
+                return line.match(/\.js:\d+:\d+\)$/);
+              });
+            }
             node.textContent = node.previousSibling.textContent + '\n' + stack.join('\n');
           });
         })(traces[i]);
