9ea1ef29d79cb2ea5fb1f7a5a8af72c63347f644
Ryan Dy <rdy@pivotal.io>
2016-04-27 23:13:31 -0700

    try to use phantomjs-prebuilt when available

--- src/lib/drivers/phantomjs.js
+++ src/lib/drivers/phantomjs.js
@@ -2,9 +2,13 @@ module.exports = function() {
   return {
     get command() {
       try {
-        return require('phantomjs').path;
+        return require('phantomjs-prebuilt').path;
       } catch(e) {
-        return 'phantomjs';
+        try {
+          return require('phantomjs').path;
+        } catch(e) {
+          return 'phantomjs';
+        }
       }
     },
     runner: 'phantom_runner.js',
