6d2cd0f4022a7530d249adbcdb5202b0edf68aa0
Vladimir Chulanov <vladimir_chulanov@epam.com>
2020-03-02 10:10:53 +0300

    Fixed issue #57 Windows: Can't run gulp-jasmine-browser with Headless Chrome.

--- src/lib/drivers/chrome.js
+++ src/lib/drivers/chrome.js
@@ -2,7 +2,14 @@ import path from 'path';
 export default function chrome() {
   return {
     get command() {
-      return path.resolve(__dirname, '../runners/chrome_runner');
+      return process.platform === 'win32'
+          ? 'node'
+          : path.resolve(__dirname, '../runners/chrome_runner');
+    },
+    get runner() {
+      return process.platform === 'win32'
+          ? 'chrome_runner'
+          : undefined;
     },
     output: 'stderr'
   };
